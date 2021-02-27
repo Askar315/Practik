c39584df967424094f326843ccab1d49013728c0
Askar Mambetaliev <mambetalievaskar2@gmail.com>
2021-02-27 08:56:02 +0600

    Задание_1_2

---
+++ Practika_1_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Practika_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "";
+            //Считывание текста с папки
+            StreamReader Practika_2 = new StreamReader("coordinates.txt");
+
+
+            Console.WriteLine("Your marked coordinates on the map:");
+            while (text != null)
+            {
+                //Читаем файловый докумет и присваиваем в text
+                text = Practika_2.ReadLine();
+                if (text != null)
+                {
+                    // вывод отформатированном вида
+                    Console.WriteLine("X:" + text.Substring(0, text.IndexOf(',')) + " Y:" + text.Substring(text.IndexOf(',')) + 1, text.Length - text.IndexOf(','));
+
+                }
+
+            }
+            //Закрытие файла
+            Practika_2.Close();
+        }
+    }
+}
