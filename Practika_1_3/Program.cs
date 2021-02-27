db142047f4c06d7f4957a5bdae4f289dd29e3b40
Askar Mambetaliev <mambetalievaskar2@gmail.com>
2021-02-27 08:58:49 +0600

    Задание_3

---
+++ Practika_1_3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace Practika_1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "";
+            //Считывание текста с папки
+            StreamReader Practika_2 = new StreamReader("coordinates.txt");
+
+
+            Console.WriteLine("Your marked coordinates on the map:");
+            while (text != null)
+            {
+                //Читаем файловый докумет и присваиваем в text
+                text = Practika_2.ReadLine();
+                if (text != null)
+                {
+                    // вывод отформатированном вида
+                    Console.WriteLine("X:" + text.Substring(0, text.IndexOf(',')) + " Y:" + text.Substring(text.IndexOf(',')) + 1, text.Length - text.IndexOf(','));
+
+                }
+
+            }
+            //Закрытие файла
+            Practika_2.Close();
+
+            string Yes = "0";
+            string NO = "1";
+
+            Console.WriteLine("Вы хотите ввести свои координаты 0 - да, 1 - нет");
+            string x = Console.ReadLine();
+
+            if (Yes == x)
+            {
+                Console.WriteLine("Введите свои 3 координаты:");
+                string q = Console.ReadLine();
+                string w = Console.ReadLine();
+                string e = Console.ReadLine();
+
+                Console.WriteLine("Отформатированном вид:");
+
+                Console.WriteLine("X:" + q.Substring(0, q.IndexOf(",")) + " Y:" + q.Substring(q.IndexOf(",")), q.Length - q.IndexOf(","));
+                Console.WriteLine("X:" + w.Substring(0, w.IndexOf(",")) + " Y:" + w.Substring(w.IndexOf(",")), w.Length - w.IndexOf(","));
+                Console.WriteLine("X:" + e.Substring(0, e.IndexOf(",")) + " Y:" + e.Substring(e.IndexOf(",")), e.Length - e.IndexOf(","));
+            }
+            else if (NO == x)
+            {
+                Console.WriteLine("Отказ прощайте!!!");
+                return;
+            }
+
+
+        }
+    }
+}
